13dc636c9263bfbbe0b9f0d2261faa940e7f3b38
roope-a <airinenroope@gmail.com>
2021-04-21 23:16:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/foodcalculator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.foodcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int something;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
